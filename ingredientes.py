4e2b4bf324a82b5d307a3ef98919e08c8042c791
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-23 18:38:25 -0400

    Update ingredientes.py
    se implementa salsas posibles

--- ingredientes.py
+++ ingredientes.py
@@ -3,3 +3,4 @@
 vegetales_posibles = ["tomate", "aceitunas", "champiñones"]
 proteicos_posibles = ["pollo", "vacuno", "carne vegetal"]
 masas_posibles = ["tradicional", "delgada"]
+salsas_posibles = ["salsa de tomate", "salsa bbq"]
