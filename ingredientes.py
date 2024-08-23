226d46f16e8b5245f78d768a801369f4e3be6c18
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-23 19:01:51 -0400

    Update ingredientes.py
    incorporación de docstring

--- ingredientes.py
+++ ingredientes.py
@@ -1,6 +1,21 @@
-# listas con valores posibles
+# ingredientes.py
 
 vegetales_posibles = ["tomate", "aceitunas", "champiñones"]
+"""
+list of str: Lista que contiene los ingredientes vegetales disponibles para la pizza.
+"""
+
 proteicos_posibles = ["pollo", "vacuno", "carne vegetal"]
+"""
+list of str: Lista que contiene los ingredientes proteicos disponibles para la pizza.
+"""
+
 masas_posibles = ["tradicional", "delgada"]
+"""
+list of str: Lista que contiene los tipos de masa disponibles para la pizza.
+"""
+
 salsas_posibles = ["salsa de tomate", "salsa bbq"]
+"""
+list of str: Lista que contiene los tipos de salsa disponibles para la pizza.
+"""
