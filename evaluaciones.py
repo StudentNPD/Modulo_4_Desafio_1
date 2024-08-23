514420608e9e005c201e72e1d98332960d9e4361
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-23 18:37:35 -0400

    Update evaluaciones.py
    puntos a y b

--- evaluaciones.py
+++ evaluaciones.py
@@ -1,1 +1,8 @@
+from pizza import Pizza
+from ingredientes import salsas_posibles
+#a
+print("Precio:", Pizza.precio)
+print("Tamaño:", Pizza.tamaño)
+#b
+print("¿Está 'salsa de tomate' en la lista? :", Pizza.validar_ingrediente("salsa de tomate",salsas_posibles))
 
