faff7815eb62449df1e7b5a1076807dcd4ce82e1
Francisco Colomer B. <francisco.colomer@gmail.com>
2024-08-23 20:11:26 -0400

    Update evaluaciones.py

--- evaluaciones.py
+++ evaluaciones.py
@@ -2,7 +2,7 @@ from pizza import Pizza
 from ingredientes import salsas_posibles
 #a
 print("Precio:", Pizza.precio)
-print("Tamaño:", Pizza.tamaño)
+print("Tamaño:", Pizza.tamano)
 #b
 print("¿Está 'salsa de tomate' en la lista? :", Pizza.validar_ingrediente("salsa de tomate",salsas_posibles))
 
