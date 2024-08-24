299b642cf8e607efc47e0d1ba464ce5cff0fbcf0
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-08-23 20:06:36 -0400

    print Ingrendientes vegetal, tipo de masa, proteina

--- evaluaciones.py
+++ evaluaciones.py
@@ -15,7 +15,7 @@ print("¿Está 'salsa de tomate' en la lista? :", Pizza.validar_ingrediente("sal
 # instanciar
 mi_pizza = Pizza()
 # llamar a la función pedido_pizza
-solicitar_ingrediente_masa = mi_pizza.pedido_pizza()
+mi_pizza.pedido_pizza()
 
 #d
 # Usar la función print(), para que al ejecutar el script, luego de que el usuario
@@ -23,11 +23,15 @@ solicitar_ingrediente_masa = mi_pizza.pedido_pizza()
 # ingredientes vegetales, el ingrediente proteico y el tipo de masa de la instancia
 # creada en el paso anterior, y si esa instancia es una pizza válida o no.
 # imprimir los atributos
-print(solicitar_ingrediente_masa)
+
+print("Ingredientes vegetal:", mi_pizza.ingredientes_vegetales)
+print("Tipo de masa:", mi_pizza.tipo_masa)
+print("Proteina:", mi_pizza.ingrediente_proteico)
+print(mi_pizza.es_valido)
 
 #e
 # Usar la función print(), para mostrar en pantalla si la clase Pizza es una pizza
 # válida o no, haciendo uso del atributo creado en el requerimiento 4, sin crear
 # una instancia de la clase. En este punto, la ejecución del script debe mostrar
 # un error (todos los pasos anteriores se deben haber ejecutado correctamente).
-print(mi_pizza.es_valido())
+print(Pizza.es_valido)
